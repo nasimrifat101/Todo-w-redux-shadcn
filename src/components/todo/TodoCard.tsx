e3d8f71431a53dfd9a3ec792d2509214e771925d
nasimrifat101 <nasimrifat101@gmail.com>
2024-07-07 21:16:47 +0600

    static todos

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,10 +1,18 @@
-
 const TodoCard = () => {
-    return (
-        <div>
-            <h1>Todo Card</h1>
+  return (
+    <>
+      <div className="bg-white rounded-xl flex justify-between items-center p-2">
+        <input type="checkbox" name="" id="" />
+        <p>Title</p>
+        <p>Time</p>
+        <p>Description</p>
+        <div className="space-x-5">
+          <button>del</button>
+          <button>edit</button>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default TodoCard;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,10 +1,21 @@
+import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <button>Add Todo</button>
+        <button>Filter</button>
+      </div>
+      <div className="bg-red-500 w-full h-[500px] rounded-xl p-5 space-y-2">
+       <TodoCard/>
+       <TodoCard/>
+       <TodoCard/>
+       <TodoCard/>
+       <TodoCard/>
+      </div>
+    </>
+  );
 };
 
 export default TodoContainer;
